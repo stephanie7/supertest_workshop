5ea0833fe1101aa49d6ccbf36b9cddd854b56e10
zhoujing280 <zhou_jing2006@163.com>
2016-12-06 14:08:09 +0800

    give a example

--- test/module/pet.js
+++ test/module/pet.js
@@ -37,45 +37,8 @@ describe('As a management for pet store, I need to manage kinds of pets.', funct
             .expect(200,done);
     });
 
-
-
-    it('The management should update the new pet information by pet_id.', function(done) {
-        var setPet = {
-                    "id": 10,
-                    "category": {
-                    "id": 11,
-                    "name": "二号宠物店"},
-                    "name": "doggie",
-                    "photoUrls": [
-                        "http://www.mypet.picture.com"],
-                    "tags": [{
-                            "id": 12,
-                            "name": "my pet"
-                        }],
-                    "status": "available"
-            }
-        request.put('/pet')
-            .send(setPet)
-            .expect(200,done)
-        });
-
-
-    it('The management should get the pet information after update by pet_id.', function(done) {
-        request.get('/pet/'+pet_id)
-            .expect(200, function(err, info){
-            info.res.body.category.name.should.equal('二号宠物店')
-            done();
-        });
-    });
-
 });
 
 
-
-describe('As a mangement I need to view the petstore order.', function() {
-    it('The mangement should get pet inventories by status.',function(done) {
-        request.get('/store/inventory')
-            .expect(200,done);
-    });
 
 
